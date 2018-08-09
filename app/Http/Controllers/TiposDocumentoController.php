621548030fc7c4a308ee394cdbc5a122d4a5f8b7
jomazao <jomazao@gmail.com>
2018-08-09 16:11:41 -0500

    primera subida

---
+++ app/Http/Controllers/TiposDocumentoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TiposDocumento;
+use Illuminate\Http\Request;
+
+class TiposDocumentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TiposDocumento  $tiposDocumento
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TiposDocumento $tiposDocumento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TiposDocumento  $tiposDocumento
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TiposDocumento $tiposDocumento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TiposDocumento  $tiposDocumento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TiposDocumento $tiposDocumento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TiposDocumento  $tiposDocumento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TiposDocumento $tiposDocumento)
+    {
+        //
+    }
+}
---
+++ database/factories/CiudadFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\ciudad::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
